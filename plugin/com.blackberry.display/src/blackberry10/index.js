5f16ca225a592bac2063e46b82bfb953f8612667
Kristoffer Flores <kflores@blackberry.com>
2014-04-16 11:58:31 -0400

    Fixed some outstanding lint errors

--- plugin/com.blackberry.display/src/blackberry10/index.js
+++ plugin/com.blackberry.display/src/blackberry10/index.js
@@ -1,18 +1,18 @@
 /*
-* Copyright (c) 2014 BlackBerry Limited.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (c) 2014 BlackBerry Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 var preventSleep,
     resultObjs = {},
@@ -29,10 +29,10 @@ module.exports = {
             views = qnx.webplatform.getWebViews(),
             handle = null,
             z = -1,
-            values,
-            i;
+            i,
+            values;
 
-        args = JSON.parse(decodeURIComponent(args.input));
+        args = JSON.parse(decodeURIComponent(args["input"]));
         for (i = 0; i < views.length; i++) {
             if (views[i].visible && views[i].zOrder > z) {
                 z = views[i].zOrder;
